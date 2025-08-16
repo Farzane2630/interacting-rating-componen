7ed41cd37e3eb080fd24bf6081dab8b27b6c2ff8
farzane <farzanekazemi8517@gmail.com>
2025-08-17 00:14:04 +0330

    refactor

--- rating.js
+++ rating.js
@@ -2,30 +2,25 @@ const options = document.querySelectorAll(".option");
 const ratingSection = document.getElementById("rating-state");
 const thankState = document.querySelector(".thank-you-state");
 const selectedRating = document.getElementById("selected-rating");
+const form = document.querySelector("form");
 
 options.forEach((option) => {
-  
   option.addEventListener("click", () => {
-    options.forEach(option => {
-      if (option.className.includes("selected")) {
-      option.classList.remove("selected");  
-      } 
-    })
-    
-    selectedRating.textContent = option.value;
-      option.classList.add("selected");
+    options.forEach((option) => {
+        option.classList.remove("selected"); // reset classlist
     });
-    
-});
 
-const form = document.querySelector("form");
+    selectedRating.textContent = option.value;
+    option.classList.add("selected");
+  });
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (selectedRating.textContent) {
     //  displaying thank you state
-    ratingSection.classList.add("display-none");
+    ratingSection.classList.add("hide");
     thankState.classList.add("display");
   } else {
     alert("you have to select a value!");
